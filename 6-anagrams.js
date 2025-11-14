e67f964380396378d9489c94a10fc3ee377e5582
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-14 11:45:23 +0000

    anagrams

---
+++ 6-anagrams.js
@@ -0,0 +1,29 @@
+function charMap(str) {
+  const charmap = {};
+  str = str.toLowerCase().replace(/[\W]/g, "");
+
+  for (let char of str) {
+    charmap[char] = ++charmap[char] || 1;
+  }
+
+  return charmap;
+}
+
+function anagrams(stringA, stringB) {
+  const charmapA = charMap(stringA);
+
+  const charmapB = charMap(stringB);
+
+  //check if the length is the same
+  if (Object.keys(charmapA).length !== Object.keys(charmapB).length)
+    return false;
+
+  //check if the values are the same
+  for (let key in charmapA) {
+    if (charmapA[key] !== charmapB[key]) return false;
+  }
+
+  return true;
+}
+
+console.log(anagrams("RAIL! SAFETY!", "liar ytefsa"));
